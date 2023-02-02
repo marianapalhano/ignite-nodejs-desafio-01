8076f8557d85d8ba9d126ebc8fba0d847c40f7af
Mariana <marianaufc@gmail.com>
2023-02-02 15:24:02 -0300

    feat(desafio-01): deleta todo

--- src/index.js
+++ src/index.js
@@ -87,7 +87,13 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find(todo => todo.id === id);
+  user.todos.splice(todo, 1);
+  
+  return response.status(200).json(user.todos);
 });
 
 module.exports = app;
